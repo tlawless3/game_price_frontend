ea4523b4e9341f9227f76e4bc9f38957a3be2ca3
Thomas Lawless <tlawless94@gmail.com>
2018-12-30 14:55:15 -0600

    React Router working

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Home, Results } from './components/index'
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        hello world
+        <Switch>
+          <Route exact path='/' render={() => (<Home />)} />
+          <Route exact path='/results' render={() => (<Results />)} />
+        </Switch>
       </div>
     );
   }
---
+++ src/components/game_info/game_info.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class GameInfo extends Component {
+  render() {
+    return (
+      <div className="App">
+        hello world
+      </div>
+    );
+  }
+}
+
+export default GameInfo
