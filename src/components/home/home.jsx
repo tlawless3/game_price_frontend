bb6560ed91349a9b7fd79b0c0f1bcdf575e554ff
Thomas Lawless <tlawless94@gmail.com>
2019-01-02 15:16:57 -0600

    touch up search functionality

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -3,6 +3,18 @@ import axios from 'axios';
 import SearchBar from '../searchBar/searchBar.jsx';
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      steamGameData: {},
+      gogGameData: {},
+      similarTitlesSteam: [],
+      similarTitlesGog: [],
+      searched: false
+    }
+
+    this.handleSearch = this.handleSearch.bind(this)
+  }
 
   async handleSearch(event, query) {
     event.preventDefault()
@@ -21,12 +33,29 @@ export default class Home extends Component {
         }
       }
     )
+
     if (steamAppId.data.length > 1) {
-      console.log(steamAppId.data)
+      this.setState({ similarTitlesSteam: steamAppId.data })
+    } else if (steamAppId.data.length === 1) {
+      const steamGame = await axios.get(process.env.REACT_APP_SERVER_URL + `/api/v1.0.0/steamgame/appid/${steamAppId.data[0].appid}`)
+      this.setState({
+        steamGameData: steamGame.data[steamAppId.data[0].appid].data
+      })
     }
+
     if (gogData.data.products.length > 1) {
-      console.log(gogData.data.products)
+      this.setState({
+        similarTitlesGog: gogData.data.products
+      })
+    } else if (gogData.data.products.length === 1) {
+      this.setState({
+        gogGameData: gogData.data.products[0]
+      })
     }
+
+    this.setState({
+      searched: true
+    })
   }
 
   render() {
