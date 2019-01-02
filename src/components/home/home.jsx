619c23e2ee01d634acf785333569ec66d822fe09
Thomas Lawless <tlawless94@gmail.com>
2019-01-02 11:39:02 -0600

    Wired up searchbar to home component

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import SearchBar from '../searchBar/searchBar.jsx';
 
 export default class Home extends Component {
+
+  handleSearch(event, query) {
+    event.preventDefault()
+    console.log(query)
+  }
+
   render() {
     return (
       <div className="home">
-        <p>home</p>
+        <SearchBar handleSearch={this.handleSearch} />
       </div>
     );
   }
