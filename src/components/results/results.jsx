5d84a16b793f833b675112532213612b44d55b39
Thomas Lawless <tlawless94@gmail.com>
2019-01-02 16:05:56 -0600

    refine searches

--- src/components/results/results.jsx
+++ src/components/results/results.jsx
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
 
-export default class Results extends Component {
-  render() {
+const Results = (props) => {
+  //if there are multiple games
+  if (props.gameData.length > 1) {
     return (
-      <div className="results">
-        <p>Hello</p>
+      <div className='results'>
+        {props.gameData.map(game => (
+          <div key={game.id}>
+            {props.platform}
+            {game.name ? game.name : game.title}
+          </div>
+        ))}
       </div>
-    );
+    )
+  }
+  //if there is only one game
+  else if (props.gameData[0].name || props.gameData[0].title) {
+    return (
+      <div className='results'>
+        {props.gameData[0].name ? props.gameData[0].name : props.gameData[0].title}
+      </div>
+    )
+  }
+  //if the game is not found
+  else {
+    return (
+      <div className='results'>
+        {`unable to find game on ${props.platform}`}
+      </div>
+    )
   }
 }
+
+export default Results
