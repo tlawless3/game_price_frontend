85c29d76667363c66aec402c7550a5ff75cef119
Thomas Lawless <tlawless94@gmail.com>
2019-01-02 14:53:54 -0600

    hook up gog query

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -7,14 +7,26 @@ export default class Home extends Component {
   async handleSearch(event, query) {
     event.preventDefault()
     query.trim()
-    const nameResult = await axios.get(
+    const steamAppId = await axios.get(
       process.env.REACT_APP_SERVER_URL + `/api/v1.0.0/steamgame/name/${query}`, {
         headers: {
           'Access-Control-Allow-Origin': '*',
         }
       }
     )
-    console.log(nameResult)
+    const gogData = await axios.get(
+      process.env.REACT_APP_SERVER_URL + `/api/v1.0.0/goggame/${query}`, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        }
+      }
+    )
+    if (steamAppId.data.length > 1) {
+      console.log(steamAppId.data)
+    }
+    if (gogData.data.products.length > 1) {
+      console.log(gogData.data.products)
+    }
   }
 
   render() {
